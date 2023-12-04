e48649d4305210f854085513a487c97d1f090012
pilarxyz <pilar@student.uns.ac.id>
2023-12-04 11:59:37 +0700

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
